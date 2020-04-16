257907c5f1e8098304fb01ec3cc789a4b2024c40
thewrath <episajout@gmail.com>
2020-04-16 11:20:44 +0200

    Add Google protobuffer messages definition

--- src/server_main.cpp
+++ src/server_main.cpp
@@ -1,3 +1,4 @@
+#include "protobuffer/build/base.pb.h"
 #include "Communication.hpp" 
 
 // This function is called by multiple thread 
@@ -9,6 +10,10 @@ void PacketProcessing(Communication::Packet packet)
 
 int main()
 {
+    // Verify that the version of the library that we linked against is
+    // compatible with the version of the headers we compiled against.
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+
     Communication::Server server(3333, std::thread::hardware_concurrency(), (Communication::processPacket) PacketProcessing);
     return 0;
 }
